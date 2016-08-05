e89c7b47a157537bf82421b254bdb85e075047b2
rajikaimal <inbox.rajika@gmail.com>
2016-08-05 11:15:14 +0530

    src -> public

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use('/', express.static('src'));
+app.use('/', express.static('public'));
 
 app.listen(port, function() {
   console.log('server running on port %s', port);
