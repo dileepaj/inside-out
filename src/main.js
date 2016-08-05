20d964e65f8ce34e1ffb75570bf1f86bdfa81409
rajikaimal <inbox.rajika@gmail.com>
2016-08-05 10:23:08 +0530

    add mui, sample appbar

--- src/main.js
+++ src/main.js
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app.js';
 
+//tap-event-plugin for touch events
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
+
 ReactDOM.render(<App />, document.getElementById('root'));
