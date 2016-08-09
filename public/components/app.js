225cf478ad581ac6419004194fc42b1979b2a83f
rajikaimal <inbox.rajika@gmail.com>
2016-08-09 10:01:06 +0530

    tweaks

--- public/components/app.js
+++ public/components/app.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
 
 const App = React.createClass({
+	getInitialState: function() {
+		this.setState({
+			open: true
+		});
+	},
+
 	render: function() {
-		return (
-			<MuiThemeProvider>
-				<div>
-					<AppBar
-					title="Inside-out"
-					iconClassNameRight="muidocs-icon-navigation-expand-more"
-					/>
-			    </div>
-			</MuiThemeProvider>
+		return (			
+			<div>
+				<AppBar
+				title="Inside-out"
+				iconClassNameRight="muidocs-icon-navigation-expand-more"
+				/>
+				<RaisedButton
+		          label="Open Drawer"
+		          onTouchTap={this.handleToggle}
+		        />
+		        <Drawer
+		          docked={false}
+		          width={200}
+		          open={this.state.open}
+		          onRequestChange={(open) => this.setState({open})}
+		        >
+		          <MenuItem onTouchTap={this.handleClose}>Menu Item</MenuItem>
+		          <MenuItem onTouchTap={this.handleClose}>Menu Item 2</MenuItem>
+		        </Drawer>
+		    </div>
 		)
 	}
 });
--- public/main.js
+++ public/main.js
@@ -1,16 +1,19 @@
+/* entry point for react application */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app.js';
 //import react-router
 import { Router, Route, browserHistory } from 'react-router'
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 //tap-event-plugin for touch events
 import injectTapEventPlugin from 'react-tap-event-plugin';
-
 injectTapEventPlugin();
 
 ReactDOM.render((
-	<Router history={browserHistory}>
-	    <Route path="/" component={App} />
-  	</Router>
+	<MuiThemeProvider>
+		<Router history={browserHistory}>
+		    <Route path="/" component={App} />
+	  	</Router>
+	</MuiThemeProvider>
 ), document.getElementById('root'));
