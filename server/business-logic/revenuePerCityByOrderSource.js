8cd632e8cea89f1c5bca582277698ded5047ddbf
DewmiR <dewmirandika@gmail.com>
2016-08-10 06:20:17 -0700

    revenue per city by order source-complete

--- server/business-logic/revenuePerCityByOrderSource.js
+++ server/business-logic/revenuePerCityByOrderSource.js
@@ -4,105 +4,52 @@
 'use strict'
 
 const sampleData = require('../orders.json');
-const keyMaps = require('../mappers/mainObject.js');
-const _ = require('underscore');
+const keyMap = require('../mappers/mainObject.js');
+const logger = require('../utils/logger');
 
 module.exports.revPerCityByOrderSource = function () {
 
-    let city = [];
-    let result = [];
+    let revenueObj = {};
+
+    sampleData.map(data => {
+        try {
 
 
-        try{
-            sampleData.map((data) => {
-                 result = _.where(city,{city : data[keyMaps.city]});
+             if(typeof revenueObj[data[keyMap.orderSource]] == 'undefined' ){
 
-                 console.log(result);
+                 revenueObj[data[keyMap.orderSource]] = {};
 
-                if(city.length == 0 || result.length == 0){
+                 if(typeof revenueObj[data[keyMap.orderSource]][data[keyMap.city]] == 'undefined'){
+                    revenueObj[data[keyMap.orderSource]][data[keyMap.city]] = {};
+                    revenueObj[data[keyMap.orderSource]][data[keyMap.city]]['totRevenue'] = Number(data[keyMap.payment]);
 
-                    let temp = {};
+                 }
+                 else{
+                     revenueObj[data[keyMap.orderSource]][data[keyMap.city]]['totRevenue'] += Number(data[keyMap.payment]);
+                 }
+             }
+            else{
+                 if(typeof revenueObj[data[keyMap.orderSource]][data[keyMap.city]] == 'undefined'){
+                     revenueObj[data[keyMap.orderSource]][data[keyMap.city]] = {};
+                     revenueObj[data[keyMap.orderSource]][data[keyMap.city]]['totRevenue'] = Number(data[keyMap.payment]);
 
-                    temp["city"] = data[keyMaps.city];
-
-                    temp["tmall"] = 0;
-                    temp["JD"] = 0;
-                    temp["YHD"] = 0;
-
-
-                    let orderSource = data[keyMaps.orderSource];
-
-                    if(orderSource == "Tmall"){
-
-                        temp["tmall"] = parseFloat(data[keyMaps.payment]);
-                    }
-                    else if(orderSource == "JD") {
-
-                        temp["JD"] = parseFloat(data[keyMaps.payment]);
-
-                    }else if(orderSource == "YHD"){
-
-                        temp["YHD"] = parseFloat(data[keyMaps.payment]);
-                    }
-
-                    city.push(temp);
-
-                }else{
-                    city.map((value) => {
-                        if(value.city == result[0].city){
-                          try {
-
-                               //console.log(value);
-
-                                     //let orderSrc = value.orderSource;
+                 }
+                 else{
+                     revenueObj[data[keyMap.orderSource]][data[keyMap.city]]['totRevenue'] += Number(data[keyMap.payment]);
+                 }
 
 
-                                     /* city.forEach(function (arrayItem) {
-                                          var sources = [];
-                                          sources.push(arrayItem.orderSources);
-
-                                        sources.forEach(function (arrayItem) {
-
-                                              console.log(arrayItem);
-
-                                             if (arrayItem.name == "Tmall") {
-
-                                                 console.log(arrayItem[0]);
-                                                 arrayItem.totalRevenue += parseFloat(data[keyMaps.payment]);
-                                                 console.log("blah");
-                                             }
-                                             else if (arrayItem.name == "JD") {
-                                                 arrayItem.totalRevenue += parseFloat(data[keyMaps.payment]);
-                                             }
-                                             else if (arrayItem.name == "YHD") {
-                                                 arrayItem.totalRevenue += parseFloat(data[keyMaps.payment]);
-                                             }
-
-                                         });
+             }
 
 
-                                      });*/
-
-                              //console.log("value city" + value.city);
-                              //console.log("result 0" + (result[0].city));
-
-                          }catch (exception){
-                              console.log(exception);
-                          }
-
-                        }
-                    });
-                }
-
-            });
-
-            //console.log(city);
-            return city;
-
-        }catch(exception){
-            console.log(exception);
         }
+        catch(exp) {
+            logger.log('error', exp + "", {stack: exp.stack});
+            throw exp + "";
+        }
+    });
 
+    return revenueObj;
 
 };
 
