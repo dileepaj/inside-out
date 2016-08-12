fd67dd827aa257948541b385493c051bdbd11b90
DewmiR <dewmirandika@gmail.com>
2016-08-11 22:34:24 -0700

    revenue per city by order source - api doc update

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -119,7 +119,7 @@ router.get('/total-customers-per-city', function(req, res) {
 });
 
 /**
- * @api {get} /rev-perCity-by-orderSource Get data for total revenue per city by order source
+ * @api {get} /rev-per-city-by-orderSource Get data for total revenue per city by order source
  * @apiName revenuePerCityByOrderSource
  * @apiGroup Data
  *
@@ -135,7 +135,7 @@ router.get('/total-customers-per-city', function(req, res) {
 	}
 }
  */
-router.get('/rev-perCity-by-orderSource', function(req, res) {
+router.get('/rev-per-city-by-orderSource', function(req, res) {
 
 	try {
 		let returnJson = revenuePerCityByOrderSource.revPerCityByOrderSource();
