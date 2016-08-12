1f66207e6d19fb2e5b0408e3f5cce5be3386975f
Deleepa <deleepa.p@gmail.com>
2016-08-12 11:13:53 +0530

    - Updated the API documentation for all endpoints

--- doc/api_data.js
+++ doc/api_data.js
@@ -39,6 +39,44 @@ define({ "api": [
   },
   {
     "type": "get",
+    "url": "/customer-engagement-pattern",
+    "title": "Customer engagement pattern",
+    "name": "CustomerEngagementPattern",
+    "group": "Analytics",
+    "description": "<p>Endpoint generates a dataset that checks customer engagement patterns by calculating standard deviations between purchases. Data is specifically formatted for HighCharts' 3D graph.</p>",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Boolean",
+            "optional": false,
+            "field": "status",
+            "description": "<p>If the calculations were successful or not</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Object",
+            "optional": false,
+            "field": "message",
+            "description": "<p>An object containing all the required data</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "HTTP/1.1 200 OK\n{\n\t\"status\": true,\n\t\"message\": {\n      \t\"name\": day of the week,\n      \t\"values\": [\n        {\n          \"x\": time interval,\n          \"timeSlot\": 0,\n          \"y\": number of purchases\n        },\n\t}\n}",
+          "type": "Object"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "server/controllers/api.js",
+    "groupTitle": "Analytics"
+  },
+  {
+    "type": "get",
     "url": "/purchases-by-days",
     "title": "Get data for total purchase amount for every day of the week",
     "name": "PurchasesByDay",
@@ -67,6 +105,120 @@ define({ "api": [
         {
           "title": "Success-Response:",
           "content": "HTTP/1.1 200 OK\n{\n\t\"status\": true,\n\t\"message\": {\n\t\t\"sunday\": 1456,\n\t\t\"monday\": 4321,\n\t\t... \n\t}\n}",
+          "type": "Object"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "server/controllers/api.js",
+    "groupTitle": "Analytics"
+  },
+  {
+    "type": "get",
+    "url": "/purchases-by-time",
+    "title": "Purchases by time",
+    "name": "PurchasesByTime",
+    "group": "Analytics",
+    "description": "<p>Calculates the amount of purchases made on each day of the week during given time intervals. The data has been specifically formatted for d3 graphs.</p>",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Boolean",
+            "optional": false,
+            "field": "status",
+            "description": "<p>If the calculations were successful or not</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Object",
+            "optional": false,
+            "field": "message",
+            "description": "<p>An object containing all the required data</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "HTTP/1.1 200 OK\n{\n\t\"status\": true,\n\t\"message\": {\n      \t\"name\": day of the week,\n      \t\"values\": [\n        {\n          \"x\": time interval,\n          \"timeSlot\": 0,\n          \"y\": number of purchases\n        },\n\t}\n}",
+          "type": "Object"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "server/controllers/api.js",
+    "groupTitle": "Analytics"
+  },
+  {
+    "type": "get",
+    "url": "/total-customers-per-city",
+    "title": "Total customers in each city",
+    "name": "TotalCustomersPerCity",
+    "group": "Analytics",
+    "description": "<p>Calculates and returns the total number of customers per city</p>",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Boolean",
+            "optional": false,
+            "field": "status",
+            "description": "<p>If the calculations were successful or not</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Object",
+            "optional": false,
+            "field": "message",
+            "description": "<p>An object containing all the required data</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "HTTP/1.1 200 OK\n{\n\t\"status\": true,\n\t\"message\": {\n      \t\t\"city\": \"...\",\n      \t\t\"amount\": ...\n\t\t}\n\t}\n}",
+          "type": "Object"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "server/controllers/api.js",
+    "groupTitle": "Analytics"
+  },
+  {
+    "type": "get",
+    "url": "/total-revenue-per-city",
+    "title": "Total revenue per city",
+    "name": "TotalRevenuePerCity",
+    "group": "Analytics",
+    "description": "<p>Calculates and returns the total revenue per city</p>",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Boolean",
+            "optional": false,
+            "field": "status",
+            "description": "<p>If the calculations were successful or not</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Object",
+            "optional": false,
+            "field": "message",
+            "description": "<p>An object containing all the required data</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "HTTP/1.1 200 OK\n{\n\t\"status\": true,\n\t\"message\": {\n      \t\t\"city\": \"...\",\n      \t\t\"revenue\": ...\n\t\t}\n\t}\n}",
           "type": "Object"
         }
       ]
